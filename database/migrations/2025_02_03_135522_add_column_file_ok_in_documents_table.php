3066b2dda7b504e0dd9725c89916bf5888b0b849
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-02-03 14:05:57 -0300

    .

---
+++ database/migrations/2025_02_03_135522_add_column_file_ok_in_documents_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('documents', function (Blueprint $table) {
+            $table->boolean('file_ok')->default(1)->comment('Coluna para controle na reinserção após falha do S3');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('documents', function (Blueprint $table) {
+            $table->dropColumn('file_ok');
+        });
+    }
+};
